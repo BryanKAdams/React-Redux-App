03dcb57efb1e79fd8ed3c8fca57eb3c845e5385d
BryanKAdams <17adamsbryk@washk12.org>
2019-12-12 15:35:07 -0700

    Added reducers, actions folders and wrapped App in provider

--- react-redux-app/src/index.js
+++ react-redux-app/src/index.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Import Reducer 
+import reducer from './reducers';
+//created a store that receives reducer and middleware parameters
+//thunk calls dispatch on functions to turn them into objects
+//for the reducer to handle
+const store = createStore(reducer, applyMiddleware(thunk));
+
+
+ReactDOM.render(
+    //Wrap App Component in Provider component and give it a store
+    <Provider>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
